85823a794027f7233f4081e4dbfaaec9fc5e5eca
eboueleamana <chriscome04@gmail.com>
2021-09-25 19:31:50 +0200

    réorganisation des templates

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home_view, name='home'),
     path('contact/',views.contact_view, name='contact'),
-    path('articles/',views.articles_view, name='articles'),
+    path('',include('articles.urls'), name='articles'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from .db_articles import articles
+
 
 def home_view(request):
      #return HttpResponse('Hello World')
@@ -11,6 +11,3 @@ def contact_view(request):
     #return HttpResponse('Contactez nous')
     return render(request, 'contact.html')
 
-
-def articles_view(request):
-    return render(request, 'articles.html', context={'articles': articles})
